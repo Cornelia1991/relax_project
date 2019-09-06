c4427a8a1df7e19a73e5ec0bd2ebef361a32c4c9
Cornelia1991 <Cornelia1991@users.noreply.github.com>
2019-09-06 09:04:49 -0400

    made rating review buttons interactive

--- massage.js
+++ massage.js
@@ -1,5 +1,5 @@
 //variables for write a rating
-const starBtns = document.querySelectorAll('.starBtn');
+
 
 //variables for write a review
 const submitReviewBtn = document.getElementById('submitReviewBtn');
@@ -10,12 +10,24 @@ const reviewResponse = document.getElementById('reviewResponse');
 
 //average rating
 //Goal highlight star rating with star button click and all stars less than star rating
-starBtns.forEach(hightlightStars);
-function hightlightStars() {
-    for (i = 0; i < starBtns.length; ++i) {
-        starBtns[i].style.backgroundColor = "#ffd700";
-    }
-}
+let count;
+function starmark(item)
+{
+    count=item.id[0];
+    sessionStorage.starRating = count;
+    let subid= item.id.substring(1);
+    for(let i=0;i<5;i++){
+        if(i<count){
+        document.getElementById((i+1)+subid).style.backgroundColor="#ffd700";
+        }else{
+        document.getElementById((i+1)+subid).style.color="black";
+        }
+    }//end for loop
+}//end star function
+    
+submitRating.addEventListener('click', event => {
+    reviewResponse.innerHTML = '<p>Thank You For Submitting A Review</br> We Value Your Input</p>';
+});
 
 
 
